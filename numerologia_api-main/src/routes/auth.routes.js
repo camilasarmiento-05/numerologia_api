bff1bc6bafd4f95b2aaea68c1ad0119b34e1d931
camilasarmiento-05 <sarmientolopezcamila05@gmail.com>
2026-09-01 13:29:49 -0500

    Delete numerologia_api-main directory

--- numerologia_api-main/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../controllers/auth.controller');
-const { validarRegistro, validarLogin } = require('../middlewares/validate.middleware');
-
-router.post('/register', validarRegistro, register);
-router.post('/login', validarLogin, login);
-
-module.exports = router;
--- numerologia_api-main/src/routes/numerology.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { calculate, getProfile } = require('../controllers/numerology.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
-
-router.post('/calculate', authMiddleware, calculate);
-router.get('/profile', authMiddleware, getProfile);
-
-module.exports = router;
